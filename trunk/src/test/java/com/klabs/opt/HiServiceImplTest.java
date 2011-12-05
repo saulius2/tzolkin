92c4998695a1024cb82bf01d3ab036b05e29d0b7
krimtter@gmail.com <krimtter@gmail.com@563a3949-dcd7-7bd6-9124-bf6594db9b29>
2011-12-05 04:21:36 +0000

    [00001] Testing and refractoring

---
+++ trunk/src/test/java/com/klabs/opt/HiServiceImplTest.java
@@ -0,0 +1,24 @@
+package com.klabs.opt;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class HiServiceImplTest {
+
+	@Test
+	public void testHi() {
+
+		ClassPathXmlApplicationContext appContext = new ClassPathXmlApplicationContext("tzolkin-config.xml");
+
+		HiService hiService = (HiService) appContext.getBean("HiService");
+		String prefix = "¡Hola ";
+		String suffix = "!, ¿como va todo? atte. Karim";
+		String name = "Karim";
+		assertEquals(prefix+name+suffix , hiService.hi(name));
+		
+		
+	}
+
+}
